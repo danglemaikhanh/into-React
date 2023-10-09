d668c6cadb799ed873da3be55c00ba58f752e54c
Mai Le <s86123@bht-berlin.de>
2023-10-09 15:50:24 +0200

    navbar done

--- Navbar-Tut/src/components/Navbar.js
+++ Navbar-Tut/src/components/Navbar.js
@@ -1,22 +1,28 @@
 import React from "react";
-import { FaBars } from "react-icons/fa"
+import { FaBars, FaTimes } from "react-icons/fa"
 import '../components/navbar.css';
 import Logo from "../images/logo.png"
 
 export default function Navbar() {
+    const [menuClick, setMenuClick] = React.useState(false)
+
+    function handleClick() {
+        setMenuClick(!menuClick)
+    }
+
     return (
         <nav className="navbar">
             <div className="navbar__logo">
                 <img src={Logo} alt="logo" />
             </div>
-            <ul className="navbar__list">
+            <ul className={menuClick ? "navbar__list active": "navbar__list"}>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#fqa">FQA</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
-            <div className="navbar__hamburger">
-                <FaBars size={30} style={{ color: '#65e1e9' }} />
+            <div className="navbar__hamburger" onClick={handleClick}>
+                {menuClick ? (<FaTimes size={30} style={{ color: '#65e1e9' }} />) : (<FaBars size={30} style={{ color: '#65e1e9' }} />)}
             </div>
         </nav>
     )
