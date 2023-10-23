2a7dc01f0a1e973b0c2a2a5074fd052e909f54c5
Mai Le <s86123@bht-berlin.de>
2023-10-23 16:34:41 +0200

    small-game done

--- small-game/src/App.js
+++ small-game/src/App.js
@@ -1,20 +1,69 @@
 import React from "react"
 import Die from "./Die"
+import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
+
 export default function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+  const [tenzies, setTenZies] = React.useState(false)
+  const [count, setCount] = React.useState(0)
+
+  React.useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const allSameValue = dice.every(die => die.value === dice[0].value)
+    if (allHeld && allSameValue) {
+      setTenZies(true)
+    }
+  }, [dice])
+
+  function generalNewDice() {
+    return {
+      id: nanoid(),
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false
+    }
+  }
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generalNewDice())
+    }
+    return newDice
+  }
+
+  function rollDice() {
+    if (tenzies) {
+      setTenZies(!tenzies)
+      setDice(allNewDice)
+      setCount(0)
+    } else {
+      setCount(prevCount => prevCount += 1)
+      setDice(prevDice => prevDice.map(die => {
+        return die.isHeld ? die : generalNewDice()
+      }))
+    }
+  }
+
+  function holdDice(id) {
+    setDice(prevDice => prevDice.map(die => {
+      return die.id === id ? { ...die, isHeld: !die.isHeld } : die
+    }))
+  }
+
+  const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
+
   return (
-    <main className="flex">
-      <div className="container">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="7" />
-        <Die value="8" />
-        <Die value="9" />
-        <Die value="10" />
+    <main>
+      {tenzies && <Confetti />}
+      <div className="text-container">
+        <h1>Tenzies</h1>
+        <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       </div>
+      <div className="container">
+        {diceElements}
+      </div>
+      <p>Roll-Count: {count}</p>
+      <button className="roll-btn" onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
     </main>
   );
 }
--- small-game/src/Die.js
+++ small-game/src/Die.js
@@ -1,9 +1,12 @@
 import React from "react"
 
 export default function Die(props) {
+    const greenBg = {
+        backgroundColor: props.isHeld ? "#59e391" : "white"
+    }
     return (
-        <div className="dies flex">
-            <h1>{props.value}</h1>
+        <div className="dies" style={greenBg} onClick={props.holdDice}>
+            <h2 className="die-num">{props.value}</h2>
         </div>
     )
 }
